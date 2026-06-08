1b4b6d53f0344439ebd8625578eca548d1de1883
Tharcio Santos <tharcio09@hotmail.com>
2026-06-08 20:49:41 -0300

    fix: atualiza domínio permitido no CORS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://frontend-api-weld.vercel.app'
+        'https://devlinks-web-api.vercel.app/'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
