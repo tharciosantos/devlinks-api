acace8831ee0d9a058cb530c38cc708efcfc93ac
Tharcio Santos <tharcio09@hotmail.com>
2026-06-08 21:00:04 -0300

    fix: libera novo domínio do frontend no CORS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://devlinks-web-api.vercel.app/'
+        'https://devlinks-web-api.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
